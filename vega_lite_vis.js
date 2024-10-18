69c73822cac3d15cd45bb07ffdf139432e51a064
Ryuji Takamura <110015984+Ryuji-arch@users.noreply.github.com>
2024-10-18 13:08:07 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -11,4 +11,17 @@ vegaEmbed("#area-chart", vg_3).then(function(result) {
     console.log("Area chart successfully loaded.");
 }).catch(console.error);
 
-    
+var vg_4 = "access-to-clean-fuels-vs-gdp-per-capita.json";
+vegaEmbed("#scatter-plot", vg_4).then(function(result) {
+    console.log("Scatter plot successfully loaded.");
+}).catch(console.error);
+
+var vg_5 = "population-using-solid-fuels-for-cooking.json";
+vegaEmbed("#slope-chart", vg_5).then(function(result) {
+    console.log("Slope chart successfully loaded.");
+}).catch(console.error);
+
+var vg_6 = "per-capita-energy-use.json";
+vegaEmbed("#bar-chart", vg_6).then(function(result) {
+    console.log("Bar chart successfully loaded.");
+}).catch(console.error);
